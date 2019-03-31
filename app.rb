4549e83f523cb58e1da4d1e6f36bd7fca3e28d9b
whosAle <abelgrave95@gmail.com>
2019-03-31 17:42:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    status 200
+    "My name is Roos."
+
+  end
+
+  get '/hometown' do
+    status 200
+    "My hometown is Botham."
+
+  end
+
+  get '/favorite-song' do
+    status 200
+    "My favorite song is 'ghostbusters'."
+
+  end
 end
