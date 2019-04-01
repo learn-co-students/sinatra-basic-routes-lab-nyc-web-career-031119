3b1256ecd00ef743bf5be15f1039dffcae14c1b1
Yevgeniy <yevgeniyerot@gmail.com>
2019-04-01 09:56:11 -0400

    Added routes

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Yevgeniy"
+  end
+
+  get '/hometown' do
+    "My hometown is Kyiv"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is In The End"
+  end
 end
