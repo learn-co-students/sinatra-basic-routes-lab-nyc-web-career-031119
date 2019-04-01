09918487d8c63467bba4b2db660fb4bf3030e1be
danielmartin0126 <danielmartin0126@gmail.com>
2019-04-01 13:14:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    'My name is why do you need to now that'
+  end
+
+  get '/hometown' do
+    'My hometown is none of your damn business'
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Total Eclipse of the Heart' by Bonnie Tyler"
+  end
 end
